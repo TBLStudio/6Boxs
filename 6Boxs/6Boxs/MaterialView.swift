16b3a7a4578b9bf88c7aee2a335c242079694ded
TBLStudio <iosdev.tbl@gmail.com>
2016-06-07 16:57:12 +0700

    6Boxes: Setup MainVC
    Setup MainVC

---
+++ 6Boxs/6Boxs/MaterialView.swift
@@ -0,0 +1,37 @@
+//
+//  MaterialView.swift
+//  6Boxs
+//
+//  Created by Thái Bô Lão on 6/7/16.
+//  Copyright © 2016 Thái Bô Lão. All rights reserved.
+//
+
+import UIKit
+
+private var materialKey = false
+extension UIView {
+    
+    @IBInspectable var materialDesign: Bool {
+        get {
+            return materialKey
+        }
+        set {
+            materialKey = newValue
+            
+            if materialKey {
+                self.layer.masksToBounds = false
+                self.layer.cornerRadius = 3.0
+                self.layer.shadowOpacity = 0.8
+                self.layer.shadowRadius = 3.0
+                self.layer.shadowOffset = CGSizeMake(0.0, 2.0)
+                self.layer.shadowColor = UIColor(red: 157/255, green: 157/255, blue: 157/255, alpha: 1).CGColor
+            } else {
+                self.layer.cornerRadius = 0
+                self.layer.shadowOpacity = 0
+                self.layer.shadowRadius = 0
+                self.layer.shadowColor = nil
+            }
+        }
+    }
+    
+}
